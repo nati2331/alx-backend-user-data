29da4dfdc48038817d948965ab9bd864524d8fbd
nati2331 <natnael.mk2@gmail.com>
2024-11-14 10:32:18 +0000

    first commit

---
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Index views.
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """Return the status of the API.
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """returs the number of each objects.
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """Unauthorized error.
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """Forbidden error.
+    """
+    abort(403)
